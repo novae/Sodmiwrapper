90ae1cb83292b56d00a864c7e343a1a674b17b4e
novae <novaegr@gmail.com>
2014-02-12 23:44:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SodmiWrapperEngine::Application.config.secret_key_base = '11f3cca75f6033f7b7167915bed34a44236c6b3378d4f1ceb9e548ec6d5241c06deac5c6733e8edcdfed5760db0be6d9dcd3485fc8b8915f17f9b042283afad5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SodmiWrapperEngine::Application.config.session_store :cookie_store, key: '_sodmiWrapperEngine_session'
